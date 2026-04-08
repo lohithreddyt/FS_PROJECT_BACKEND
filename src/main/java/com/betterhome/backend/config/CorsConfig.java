bb0ce0eb17935e6851caeae5ed4d055c8563493b
lohithreddyt <lohithreddy364@gmail.com>
2026-04-08 15:06:44 +0530

    backend

--- src/main/java/com/betterhome/backend/config/CorsConfig.java
+++ src/main/java/com/betterhome/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // frontend URL
+                        .allowedOrigins("http://localhost:3000") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
